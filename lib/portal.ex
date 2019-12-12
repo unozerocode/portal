c69958244da86f18b8f3ac1bc8a47da6d734aff9
Uno Zero Code <57536442+unozerocode@users.noreply.github.com>
2019-12-12 15:17:41 -0600

    transfers

--- lib/portal.ex
+++ lib/portal.ex
@@ -1,18 +1,33 @@
 defmodule Portal do
-  @moduledoc """
-  Documentation for Portal.
+  defstruct [:left, :right]
+
+@doc """
+  Starts transferring 'data' from left to right
+"""
+def transfer(left, right, data) do
+  # First add all the data to the portal on the left
+  for item <- data do
+    Portal.Door.push(left, item)
+  end
+
+  # Return a portal struct
+  %Portal{left: left, right: right}
+end
+
+@doc """
+  Pushes data to the right in the given portal
   """
+  def push_right(portal) do
+    # See if we can pop data from the left
+    # if so, ´push it to the right
+    # if not, do nothing
 
-  @doc """
-  Hello world.
+    case Portal.Door.pop(portal.left) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
 
-  ## Examples
-
-      iex> Portal.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    # return the portal itself
+    portal
   end
 end
--- test/portal_test.exs
+++ test/portal_test.exs
@@ -1,8 +1,36 @@
 defmodule PortalTest do
   use ExUnit.Case
+  require Portal.Door
+
   doctest Portal
 
-  test "greets the world" do
-    assert Portal.hello() == :world
+  test "Starts empty" do
+    Portal.Door.start_link(:pink)
+    assert (Portal.Door.get(:pink) == [])
+  end
+
+  test "Push a value" do
+    Portal.Door.start_link(:blue)
+    Portal.Door.push(:blue, 3)
+    assert (Portal.Door.pop(:blue) == {:ok,3})
+  end
+
+  test "Pop empty is error" do
+    Portal.Door.start_link(:green)
+    assert (Portal.Door.pop(:green) == :error)
+  end
+
+  test "Transfer" do
+    Portal.Door.start_link(:red)
+    Portal.Door.start_link(:green)
+
+    portal = Portal.transfer(:red, :green, [1,2,3]) # Connect the portals
+    # Check left portal
+    assert(Portal.Door.get(:red) == [3,2,1])
+    # Push right
+    Portal.push_right(portal)
+    assert(Portal.Door.get(:red) == [2,1])
+    assert(Portal.Door.get(:green) == [3])
+
   end
 end
